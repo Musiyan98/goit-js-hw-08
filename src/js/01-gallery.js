e8486ef8c288ad0e1fa626ef82ead4d7228cd50b
Musiyan98 <musiyan98@gmail.com>
2022-09-27 19:46:37 +0300

    task1Done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+createGalleryGrid(galleryItems);
+
+function createGalleryGrid(obj) {
+  const galleryGrid = obj.map(createGalleryItem);
+  galleryBox.insertAdjacentHTML('beforeend', galleryGrid.join(' '));
+}
+
+function createGalleryItem({ preview, original, description }) {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+
+new SimpleLightbox('.gallery a', {
+  animationSpeed: 150,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
